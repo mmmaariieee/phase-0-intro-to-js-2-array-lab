89dc217c0eeb6063ca87d7d61ca3f2eeb62d48d4
Mariia <m.goncharenkoo@icloud.com>
2022-09-23 23:11:40 -0400

    commit tree 9470d50d8f25c3f913d0c97e3962c898ce5d7d0b

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const  cats =["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(cat) {
+    cats.push(cat);
+}
+
+function destructivelyPrependCat(cat) {
+    cats.unshift(cat);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function  destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(cat) {
+    const newCats = [...cats, cat];
+    return newCats;
+}
+
+function prependCat(cat) {
+    const newCats = [cat, ...cats];
+    return newCats;
+}
+
+function removeLastCat() {
+    const copyOfCats = cats.slice(0, cats.length - 1);
+    return copyOfCats;
+}
+
+function removeFirstCat() {
+    const copyOfCats = cats.slice(1);
+    return copyOfCats;
+}
+
